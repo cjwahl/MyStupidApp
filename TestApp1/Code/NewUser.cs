27775e303489ed5ab4ce1638ac4c60357026ca2d
Chris <chris@chris.com>
2016-07-03 14:17:35 -0500

    Adds an object to populate with new user information.

--- TestApp1/Code/NewUser.cs
+++ TestApp1/Code/NewUser.cs
@@ -1,19 +1,28 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-using Android.Provider;
+using SQLite;
+using Mono.Data.Sqlite;
+using Android.Renderscripts;
 
 namespace TestApp1.Code
 {
-    public class NewUser
+    public class User
     {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string CharacterName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set;}
+        public SqliteConnection dbConnection { get; set; }
 
+
+        public static string CreateDatabase()
+        {
+            var docsFolder = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+            var pathToDatabase = System.IO.Path.Combine(docsFolder, "db_adonet.db");
+            SqliteConnection.CreateFile(pathToDatabase);
+
+            return pathToDatabase;
+        }
     }
+
+    
 }
